38ce620686e8ab763938dc4a42d161eea42f422d
gabriel messias <gabrielmessias657@gmail.com>
2025-03-10 17:48:54 -0300

    firts commit

---
+++ Publicando-Sua-API-REST/src/test/java/dio/me/PublicandoSuaApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicandoSuaApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
